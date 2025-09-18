0ebc543403e47b6250a748efd9b489ad1eed9859
Evgeniy <evgen.ostromyslenskiy@gmail.ru>
2025-09-18 12:49:18 +0300

    A

---
+++ test/database.php
@@ -0,0 +1,56 @@
+<?
+ Class Base
+ {
+    protected $serverName;
+    protected $dbName;
+    protected $userName;
+    protected $password;
+    
+    public function __construct() {
+        $this->$serverName = 'localhost';
+        $this->$dbName = 'test';
+        $this->$userName = 'root';
+        $this->$password = '';
+    }
+ }
+
+ Class Mybase extends Base 
+ {
+    private $sql;
+    public $connection;
+    public $items;
+
+    function dbPara() {
+        $this->connection = new PDO("mysql:host=$this->serverName;dbname=$this->dbName;", $this->userName, $this->password);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    }
+
+    function selectId($tableName, $rowName, $value) {
+        $this->sql = "SELECT * FROM $tableName WHERE $rowName = :$value";
+    }
+
+    function selectRow($tableName, $rowName, $offset, $limit) {
+        $this->sql = "SELECT * FROM $tableName ORDER BY $rowName DESC LIMIT :$offset, $limit";
+    }
+
+    function selectPage($count, $tableName) {
+        $this->sql = "SELECT COUNT(*) as $count FROM $tableName";
+    }
+
+    function executeStatementParam($rowName, $offset) {
+        $stmt = $this->connection->prepare($this->sql);
+        $stmt->bindParam($rowName, $offset, PDO::PARAM_INT);
+        $stmt->execute();
+        $this->items = $stmt->fetchALL(PDO::FETCH_ASSOC);
+        return $this->items;
+    }
+
+    function executeStatement() {
+        $stmt = $this->connection->prepare($this->sql);
+        $stmt->execute();
+        $this->items = $stmt->fetchColumn();
+        return $this->items;
+    }
+ }
+?>
---
+++ test/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Галактический вестник</title>
+    <link rel="stylesheet" href="../test/styles/base.css">
+    <link rel="stylesheet" href="../test/styles/footer.css">
+    <link rel="stylesheet" href="../test/styles/common.css">
+</head>
+<body>
+    <header class="header _container">
+        <div class="header__logo">
+            <img src="logo/logo.png" alt="Галактический вестник">
+        </div>
+    </header>
+    <main class="main">
+        <div class="news"></div>
+    </main>
+    <footer class="footer _container _borderTop">
+        <div class="footer__copyright">
+            © 2023 — 2412 «Галактический вестник»
+        </div>
+    </footer>
+</body>
+</html>
